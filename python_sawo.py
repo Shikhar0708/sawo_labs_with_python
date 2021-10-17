de17efb1571a510f346c9ac92f04deb65c85c1d1
Shikhar Sinha <50794248+Shikhar0708@users.noreply.github.com>
2021-10-17 15:30:59 +0530

    Add files via upload

---
+++ python_sawo.py
@@ -0,0 +1,3 @@
+import webview
+webview.create_window('Integrating Savo labs api', 'https://shikhar0708.github.io/sawo_labs_with_python/')
+webview.start() #this will open the webpage in a new window
